5c35a1fe38631a1023b1ac73e8c52241ccb72ebf
Miroslav Paralyuzov <137619754+paralyuzov@users.noreply.github.com>
2025-07-24 17:28:45 +0300

    add register route with guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,16 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth.guard';
+import { guestGuard } from './core/guards/guest.guard';
 
 export const routes: Routes = [
   {
     path:'',
-    loadComponent: () => import('./features/dashboard/dashboard.component').then(m => m.DashboardComponent)
+    loadComponent: () => import('./features/dashboard/dashboard.component').then(m => m.DashboardComponent),
+    canActivate:[authGuard]
+  },
+  {
+    path:'register',
+    loadComponent: () => import('./features/register/register.component').then(m => m.RegisterComponent),
+    canActivate:[guestGuard]
   }
 ];
