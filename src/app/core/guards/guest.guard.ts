3b90e74b0e6ab6b19c44342b4b246003c33a61f6
Miroslav Paralyuzov <137619754+paralyuzov@users.noreply.github.com>
2025-07-24 17:26:58 +0300

    create guest guard

---
+++ src/app/core/guards/guest.guard.ts
@@ -0,0 +1,13 @@
+import { CanActivateFn, Router } from '@angular/router';
+import { UserService } from '../services/user.service';
+import { inject } from '@angular/core';
+
+export const guestGuard: CanActivateFn = (route, state) => {
+  const userService = inject(UserService);
+  const router = inject(Router);
+  if (userService.isLoggedIn()) {
+    router.navigate(['']);
+    return false;
+  }
+  return true;
+};
