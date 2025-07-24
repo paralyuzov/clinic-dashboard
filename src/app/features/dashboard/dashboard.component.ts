1d5bdef8f1e8951d95b606ba9cb891d769a427d3
Miroslav Paralyuzov <137619754+paralyuzov@users.noreply.github.com>
2025-07-24 17:29:56 +0300

    modify dashboard layout

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,16 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AppointmentTableComponent } from '../../shared/ui/appointment-table/appointment-table.component';
+import { PatientChartComponent } from '../../shared/ui/patient-chart/patient-chart.component';
+import { PatientTableComponent } from '../../shared/ui/patient-table/patient-table.component';
+import { DocTableComponent } from '../../shared/ui/doc-table/doc-table.component';
+import { AuthService } from '../../core/services/auth.service';
 import { NavigationComponent } from "../../shared/ui/navigation/navigation.component";
-import { AppointmentTableComponent } from "../../shared/ui/appointment-table/appointment-table.component";
-import { PatientChartComponent } from "../../shared/ui/patient-chart/patient-chart.component";
-import { PatientTableComponent } from "../../shared/ui/patient-table/patient-table.component";
-import { DocTableComponent } from "../../shared/ui/doc-table/doc-table.component";
 
 @Component({
   selector: 'app-dashboard',
-  imports: [AppointmentTableComponent, PatientChartComponent, PatientTableComponent, DocTableComponent],
+  imports: [
+    AppointmentTableComponent,
+    PatientChartComponent,
+    PatientTableComponent,
+    DocTableComponent,
+    NavigationComponent
+],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrl: './dashboard.component.css',
 })
-export class DashboardComponent {
+export class DashboardComponent  {
+  authService = inject(AuthService);
+
+
 
 }
