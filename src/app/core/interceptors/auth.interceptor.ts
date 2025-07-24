05f98da34aa57d6598ad09c70c53d936a80352eb
Miroslav Paralyuzov <137619754+paralyuzov@users.noreply.github.com>
2025-07-24 17:27:12 +0300

    create http interceptor

---
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -0,0 +1,45 @@
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { catchError, switchMap, throwError } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const authService = inject(AuthService);
+
+  if (req.url.includes('/auth/register') || req.url.includes('/auth/login')) {
+    return next(req);
+  }
+
+  const accessToken = localStorage.getItem('accessToken');
+  let authReq = req;
+  if (accessToken) {
+    authReq = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+  }
+
+  return next(authReq).pipe(
+    catchError((error: HttpErrorResponse) => {
+      if (error.status === 401) {
+        localStorage.removeItem('accessToken');
+        return authService.refreshToken().pipe(
+          switchMap((response) => {
+            if (response && response.accessToken) {
+              localStorage.setItem('accessToken', response.accessToken);
+              const newAuthReq = req.clone({
+                setHeaders: {
+                  Authorization: `Bearer ${response.accessToken}`,
+                },
+              });
+              return next(newAuthReq);
+            }
+            return throwError(() => new Error('Failed to refresh access token'));
+          })
+        );
+      }
+      return throwError(() => error);
+    })
+  );
+};
