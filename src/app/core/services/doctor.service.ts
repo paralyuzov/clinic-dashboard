c79c61855560b41b838dfb7ed16f0a67c43c71d2
Miroslav Paralyuzov <137619754+paralyuzov@users.noreply.github.com>
2025-07-26 21:57:16 +0300

    add editDoctorById and deleteDoctorById methods

--- src/app/core/services/doctor.service.ts
+++ src/app/core/services/doctor.service.ts
@@ -4,22 +4,19 @@ import { BehaviorSubject } from 'rxjs';
 import { CreateDoctorDto, Doctor } from '../models/doctor.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DoctorService {
-
-  private apiService = inject(ApiService)
+  private apiService = inject(ApiService);
 
   private doctors$$ = new BehaviorSubject<Doctor[] | null>(null);
   public readonly doctors$ = this.doctors$$.asObservable();
-  private curentDoctor$$ = new BehaviorSubject<Doctor | null>(null);
-  public readonly currentDoctor$ = this.curentDoctor$$.asObservable();
+  private currentDoctor$$ = new BehaviorSubject<Doctor | null>(null);
+  public readonly currentDoctor$ = this.currentDoctor$$.asObservable();
   private loading$$ = new BehaviorSubject<boolean>(false);
   public readonly loading$ = this.loading$$.asObservable();
   private error$$ = new BehaviorSubject<string | null>(null);
   public readonly error$ = this.error$$.asObservable();
-
-
 
   onGetDoctors() {
     this.loading$$.next(true);
@@ -33,15 +30,15 @@ export class DoctorService {
         console.error('Failed to fetch doctors:', error);
         this.error$$.next(error.message);
         this.loading$$.next(false);
-      }
-    })
+      },
+    });
   }
 
   onGetDoctorById(id: string) {
     this.loading$$.next(true);
     this.apiService.getDoctorById(id).subscribe({
       next: (response) => {
-        this.curentDoctor$$.next(response);
+        this.currentDoctor$$.next(response);
         this.error$$.next(null);
         this.loading$$.next(false);
       },
@@ -49,7 +46,7 @@ export class DoctorService {
         console.error('Failed to fetch doctor by ID:', error);
         this.error$$.next(error.message);
         this.loading$$.next(false);
-      }
+      },
     });
   }
 
@@ -66,8 +63,41 @@ export class DoctorService {
         console.error('Failed to create doctor:', error);
         this.error$$.next(error.message);
         this.loading$$.next(false);
-      }
+      },
     });
   }
 
+  onEditDoctorById(id: string, doctorData: CreateDoctorDto) {
+    this.loading$$.next(true);
+    this.apiService.editDoctorById(id, doctorData).subscribe({
+      next: (response) => {
+        this.onGetDoctors();
+        this.currentDoctor$$.next(response.doctor);
+        this.error$$.next(null);
+        this.loading$$.next(false);
+      },
+      error: (error) => {
+        console.error('Failed to edit doctor:', error);
+        this.error$$.next(error.message);
+        this.loading$$.next(false);
+      },
+    });
+  }
+
+  onDeleteDoctorById(id: string) {
+    this.loading$$.next(true);
+    this.apiService.deleteDoctorById(id).subscribe({
+      next: (response) => {
+        this.onGetDoctors();
+        this.currentDoctor$$.next(null);
+        this.error$$.next(null);
+        this.loading$$.next(false);
+      },
+      error: (error) => {
+        console.error('Failed to delete doctor:', error);
+        this.error$$.next(error.message);
+        this.loading$$.next(false);
+      },
+    });
+  }
 }
