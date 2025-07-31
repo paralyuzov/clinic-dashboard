94b2fd2b8929c6bf3998830a6cce06261f746df6
Miroslav Paralyuzov <137619754+paralyuzov@users.noreply.github.com>
2025-08-01 02:09:11 +0300

    make appoitnement table status editable

--- src/app/shared/ui/appointment-table/appointment-table.component.ts
+++ src/app/shared/ui/appointment-table/appointment-table.component.ts
@@ -14,7 +14,7 @@ export interface Appointment {
   reason: string;
   status: 'Scheduled' | 'Completed' | 'Cancelled' | 'Pending';
 }
-import { Component, inject, OnInit, ViewChild } from '@angular/core';
+import { Component, inject, OnInit, signal, ViewChild } from '@angular/core';
 import { Table, TableModule } from 'primeng/table';
 import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
@@ -24,6 +24,10 @@ import { InputIconModule } from 'primeng/inputicon';
 import { InputTextModule } from 'primeng/inputtext';
 import { AppointmentService } from '../../../core/services/appointment.service';
 import { AsyncPipe } from '@angular/common';
+import { SelectModule } from 'primeng/select';
+import { SelectItem } from 'primeng/api';
+import { TagModule } from 'primeng/tag';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-appointment-table',
@@ -36,6 +40,9 @@ import { AsyncPipe } from '@angular/common';
     InputIconModule,
     InputTextModule,
     AsyncPipe,
+    SelectModule,
+    TagModule,
+    FormsModule,
   ],
   templateUrl: './appointment-table.component.html',
   styleUrl: './appointment-table.component.css',
@@ -44,10 +51,45 @@ export class AppointmentTableComponent implements OnInit {
   @ViewChild('dt') dt: Table | undefined;
   appointmentService = inject(AppointmentService);
   appointments$ = this.appointmentService.appointments$;
+  editing = false;
+
+  statuses!: SelectItem[];
 
   ngOnInit() {
     this.appointmentService.fetchAppointments();
+    this.statuses = [
+      { label: 'Scheduled', value: 'Scheduled' },
+      { label: 'Completed', value: 'Completed' },
+      { label: 'Cancelled', value: 'Cancelled' },
+    ];
   }
+
+  getSeverity(status: string) {
+    switch (status) {
+      case 'Scheduled':
+        return 'warn';
+      case 'Completed':
+        return 'success';
+      case 'Cancelled':
+        return 'danger';
+      default:
+        return 'info';
+    }
+  }
+
+  onRowEditInit(appointment: Appointment) {
+    this.editing = true;
+  }
+
+  onRowEditCancel(appointment: Appointment, index: number) {
+
+    this.editing = false;
+  }
+
+   onRowEditSave(appointment: Appointment) {
+    this.editing = false;
+      console.log(appointment)
+    }
 
   applyFilterGlobal($event: Event, stringVal: string) {
     this.dt!.filterGlobal(($event.target as HTMLInputElement).value, stringVal);
