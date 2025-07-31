983a0054250a9e0cee7cc74caea8773cfb4daddb
Miroslav Paralyuzov <137619754+paralyuzov@users.noreply.github.com>
2025-08-01 02:08:15 +0300

    subscribe to services and update counts on data changes with cleanup

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,10 +1,13 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { AppointmentTableComponent } from '../../shared/ui/appointment-table/appointment-table.component';
 import { PatientChartComponent } from '../../shared/ui/patient-chart/patient-chart.component';
 import { PatientTableComponent } from '../../shared/ui/patient-table/patient-table.component';
 import { DocTableComponent } from '../../shared/ui/doc-table/doc-table.component';
-import { AuthService } from '../../core/services/auth.service';
-import { NavigationComponent } from "../../shared/ui/navigation/navigation.component";
+import { NavigationComponent } from '../../shared/ui/navigation/navigation.component';
+import { AppointmentService } from '../../core/services/appointment.service';
+import { Subscription } from 'rxjs';
+import { PatientService } from '../../core/services/patient.service';
+import { DoctorService } from '../../core/services/doctor.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -13,14 +16,40 @@ import { NavigationComponent } from "../../shared/ui/navigation/navigation.compo
     PatientChartComponent,
     PatientTableComponent,
     DocTableComponent,
-    NavigationComponent
-],
+    NavigationComponent,
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
-export class DashboardComponent  {
-  authService = inject(AuthService);
+export class DashboardComponent implements OnInit, OnDestroy {
+  appointmentService = inject(AppointmentService);
+  doctorService = inject(DoctorService);
+  patientService = inject(PatientService);
+  appointmentsToday: number = 0;
+  numberOfPatients: number = 0;
+  numberOfDoctors: number = 0;
+  private subscriptions = new Subscription();
 
+  ngOnInit() {
+    this.subscriptions.add(
+      this.appointmentService.appointments$.subscribe(() => {
+        this.appointmentsToday =
+          this.appointmentService.numberOfAppointmentsToday();
+      })
+    );
+    this.subscriptions.add(
+      this.doctorService.doctors$.subscribe(() => {
+        this.numberOfDoctors = this.doctorService.numberOfDoctors();
+      })
+    );
+    this.subscriptions.add(
+      this.patientService.patients$.subscribe(() => {
+        this.numberOfPatients = this.patientService.numberOfPatients();
+      })
+    );
+  }
 
-
+  ngOnDestroy() {
+    this.subscriptions.unsubscribe();
+  }
 }
