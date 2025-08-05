cf59cb45a824c6c1a0707f0102a7d4bf4ac6005d
Miroslav Paralyuzov <137619754+paralyuzov@users.noreply.github.com>
2025-08-05 03:35:05 +0300

    switch to input binding for patients and remove async pipe

--- src/app/shared/ui/patient-table/patient-table.component.ts
+++ src/app/shared/ui/patient-table/patient-table.component.ts
@@ -1,27 +1,12 @@
-import { Component, inject, ViewChild } from '@angular/core';
+import { Component, inject, input, ViewChild } from '@angular/core';
 import { TableModule, Table } from 'primeng/table';
 import { MessageModule } from 'primeng/message';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
-import { PatientService } from '../../../core/services/patient.service';
-import { AsyncPipe } from '@angular/common';
 import { CountryCodePipe } from '../../../core/pipes/country-code.pipe';
 import { DialogService } from 'primeng/dynamicdialog';
 import { PatientDetailsComponent } from '../../../features/patient-details/patient-details.component';
-
-export interface Patient {
-  id: number;
-  name: string;
-  code: string;
-  age: number;
-  gender: string;
-  phone: string;
-  email: string;
-  address: string;
-  doctor: string;
-  status: 'Active' | 'Inactive' | 'New';
-  lastVisit: string;
-}
+import { Patient } from '../../../core/models/patient.model';
 
 @Component({
   selector: 'app-patient-table',
@@ -30,7 +15,6 @@ export interface Patient {
     MessageModule,
     ButtonModule,
     InputTextModule,
-    AsyncPipe,
     CountryCodePipe,
   ],
   templateUrl: './patient-table.component.html',
@@ -39,14 +23,8 @@ export interface Patient {
 })
 export class PatientTableComponent {
   @ViewChild('dt') dt: Table | undefined;
-
-  patientService = inject(PatientService);
-  patients$ = this.patientService.patients$;
-  loading$ = this.patientService.loading$;
-  error$ = this.patientService.error$;
+  patients = input<Patient[]>([]);
   dialogService = inject(DialogService)
-
-
   openPatientDetails(id: string) {
      const ref = this.dialogService.open(PatientDetailsComponent, {
             modal: true,
