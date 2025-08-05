d0b2e84ecc2057f78fc5f96d8b033aacfb65a309
Miroslav Paralyuzov <137619754+paralyuzov@users.noreply.github.com>
2025-08-05 03:37:14 +0300

    add global toast notification

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router'
 import { LoaderComponent } from "./shared/ui/loader/loader.component";
+import {Toast} from "primeng/toast";
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoaderComponent],
+  imports: [RouterOutlet, LoaderComponent, Toast],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent{
   title = 'clinic-admin';
-
 }
