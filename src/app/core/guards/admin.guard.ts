71dd21b0c0f1af4acfeb975eaddb05da287f58a4
Miroslav Paralyuzov <137619754+paralyuzov@users.noreply.github.com>
2025-08-08 07:48:17 +0300

    add admin route guard to restrict access based on user role

---
+++ src/app/core/guards/admin.guard.ts
@@ -0,0 +1,26 @@
+import { CanActivateFn, Router } from '@angular/router';
+import { UserService } from '../services/user.service';
+import { inject } from '@angular/core';
+import { map, take, filter } from 'rxjs/operators';
+
+export const adminGuard: CanActivateFn = (route, state) => {
+  const userService = inject(UserService);
+  const router = inject(Router);
+
+  if (!userService.currentUser && localStorage.getItem('accessToken')) {
+    userService.verifyUser();
+  }
+
+  return userService.user$.pipe(
+    filter(user => user !== null),
+    take(1),
+    map(user => {
+      if (user?.role !== 'admin') {
+        router.navigate(['/employee-dashboard']);
+        return false;
+      }
+
+      return true;
+    })
+  );
+};
