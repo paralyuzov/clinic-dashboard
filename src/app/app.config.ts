60de59ab87e9c292f1ba67a4d9e2b1a94adb6384
Miroslav Paralyuzov <137619754+paralyuzov@users.noreply.github.com>
2025-08-09 10:37:29 +0300

    change primeNG theme

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,7 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
-import Lara from '@primeng/themes/lara';
+import Material from '@primeng/themes/material';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
@@ -16,11 +16,16 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
-        preset: Lara,
+        preset: Material,
+        options: {
+           darkModeSelector: 'html.dark',
+           ripple: true,
+
+        },
       },
     }),
     provideHttpClient(),
     provideHttpClient(withInterceptors([authInterceptor])),
-    MessageService
+    MessageService,
   ],
 };
